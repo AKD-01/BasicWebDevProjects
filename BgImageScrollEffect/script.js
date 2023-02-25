2cab9d773c815d858b4167f863de96408259a5e8
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-02-25 23:05:42 +0530

    update script.js

--- BgImageScrollEffect/script.js
+++ BgImageScrollEffect/script.js
@@ -1,1 +1,10 @@
-const bgImageEl=
+const bgImageEl = document.getElementById("bg-image");
+
+window.addEventListener("scroll", () => {
+    updateImage();
+});
+
+function updateImage() {
+    bgImageEl.style.opacity = 1 - window.pageYOffset/900;
+    bgImageEl.style.backgroundSize = `${160 - window.pageYOffset / 12}%`;
+}
