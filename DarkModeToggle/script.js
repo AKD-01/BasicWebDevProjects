93b95ff8c83b10284dcc8772355aa7d8e5c9495a
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-03-12 21:08:14 +0530

    update script.js

---
+++ DarkModeToggle/script.js
@@ -0,0 +1,21 @@
+const inputEl = document.querySelector(".input");
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+  } else {
+    bodyEl.style.background = "white";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
